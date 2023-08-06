4f2f64fdda72b0f219fac4c731609a8f42938852
Abel Amarilla <abelamarilla51777@gmail.com>
2023-08-05 20:39:17 -0400

    Adivina el numero

---
+++ AdivinaElNumero/adivina_el_numero_computadora.py
@@ -0,0 +1,38 @@
+import random
+
+
+def adivina_el_numero_computadora(x):
+    print("============================")
+    print("Bienvenido(a) al Juego!")
+    print("============================")
+    print(
+        f"Selecciona un numero entre 1 y {x} para que la computadora intente adivinarlo.!"
+    )
+
+    # definir limites [1, x]
+    inferior = 1
+    superior = x
+
+    respuesta = ""
+    while respuesta != "c":
+        # Generar una prediccion
+        if inferior != superior:
+            prediccion = random.randint(inferior, superior)
+        else:
+            prediccion = inferior  # podria ser tambier el limite superior
+
+        # Obtener una respuesta del usuario
+        respuesta = input(
+            f"Mi prediccion es {prediccion}. Si es muy alta, ingresa (A).Si es muy baja. ingresa (B). Si es correcta, ingresa (C): "
+        ).lower()
+
+        # Pedimos al usuario que nos diga si la respuesta es Alta, Baja o Correcta
+        if respuesta == "a":
+            superior = prediccion - 1
+        elif respuesta == "b":
+            inferior = prediccion + 1
+
+    print(f"¡Siii!! La computadora adivino tu numero correctamente: {prediccion}")
+
+
+adivina_el_numero_computadora(10)
---
+++ HistoriasLocas/historias_locas.py
@@ -0,0 +1,16 @@
+# Supongamos que queremos crear una cadena que diga:
+# Aprende a programar con_______
+# organizacion = "freecodecamp"
+# print("Aprende a programar con  " + organizacion)
+# print("Aprende a programar con {}".format(organizacion))
+# print(f"Aprende a programar con {organizacion}")  # f-string
+
+# Mad Libs (Historias Locas)
+
+adj = input("Adjetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo(plural): ")
+madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verb1} problemas. ¡Aprende a {verb2} con freecodecamp y alcanza tus {sustantivo_plural}"
+
+print(madlib)
